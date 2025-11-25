f346ee7c90e4b5faf5bef1e7ba7d2851c381ebde
dhanbyte <dhananjay.win2004@gmail.com>
2025-11-25 21:58:46 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,6 +139,17 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ubpn594hsu");
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
